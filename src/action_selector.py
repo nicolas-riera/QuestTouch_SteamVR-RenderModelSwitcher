367f1e9e50b1f880aced2140305be341e99253fd
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-10 16:21:43 +0100

    added vive controllers texture, context menu and checker

--- src/action_selector.py
+++ src/action_selector.py
@@ -8,26 +8,29 @@ from src.clear import clear
 
 def action_selector():
 
-    headset = None
+    device = None
 
     while True:
 
-        if not headset:
+        if not device:
             
             clear()
 
-            print("Please select an headset : ")
-            print("2. Quest 2")
-            print("3. Quest 3")
+            print("Please select a device : ")
+            print("1. Vive Controllers")
+            print("2. Quest 2 Controllers/Touch Quest 2")
+            print("3. Quest 3 Controllers/Touch Plus")
             print("0. Exit")
 
             usr_choice = input()
 
             match usr_choice:
+                case "1":
+                    device = "Wands"
                 case "2":
-                    headset = "Quest 2"
+                    device = "Quest 2"
                 case "3":
-                    headset = "Quest 3"
+                    device = "Quest 3"
                 case "0":
                     clear()
                     break
@@ -35,41 +38,52 @@ def action_selector():
                     clear()
                     print("Invalid Choice.")
                     input("Press Enter to Continue.")
-                    headset = None
+                    device = None
 
-        elif headset:
+        elif device:
 
-            if headset == "Quest 2":
+            if device == "Quest 2":
                 touch_name = "Touch Quest 2"
-            elif headset == "Quest 3":
+            elif device == "Quest 3":
                 touch_name = "Touch Plus"
+            elif device == "Wands":
+                text_1 = "Use Vive Controller 2.0 Render Model"
+                text_2 = "Use Vive Controller 1.0 Render Model"
 
             clear()
 
             print(f"Current SteamVR path : {real_case_path(find_steam_path())}\steamapps\common\SteamVR")
-            print(f"Current Render Model : {check_current_rendermodel(headset)}\n")
+            print(f"Current Render Model : {check_current_rendermodel(device)}\n")
 
             print("Please make a selection : ")
-            print(f"1. Replace {touch_name} Render Models with Touch Pro Render Models")
-            print(f"2. Restore {touch_name} Render Models")
-            print("3. Switch headset")
+            if device == "Quest 2" or device == "Quest 3":
+                print(f"1. Replace {touch_name} Render Models with Touch Pro Render Models")
+                print(f"2. Restore {touch_name} Render Models")
+            else:
+                print(f"1. {text_1}")
+                print(f"2. {text_2}")
+            print("3. Switch device")
             print("0. Exit")
 
             usr_choice = input()
 
             match usr_choice:
                 case "1":
-                    if headset == "Quest 2":
+                    if device == "Quest 2":
                         TouchPro_Quest2_Replace()
-                    elif headset == "Quest 3":
+                    elif device == "Quest 3":
                         TouchPro_Plus_Replace()
+                    elif device == "Wands":
+                        pass
                 case "2":
-                    if headset == "Quest 2":
+                    if device == "Quest 2":
                         TouchQuest2_Restore()
-                    elif headset == "Quest 3":
+                    elif device == "Quest 3":
                         TouchPlus_Restore()
+                    elif device == "Wands":
+                        pass
                 case "3":
-                    headset = None
+                    device = None
                     continue
                 case "0":
                     clear()
