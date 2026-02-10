4a1f2bad7d56fcf7fc17cb62e01ab5c186c94067
nicolas-riera <nicolas.riera@laplateforme.io>
2026-02-10 12:40:53 +0100

    Added gitignore and added SteamVR running check

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.action_selector import action_selector
 from src.get_SteamVR_path import get_SteamVR_path
+from src.check_SteamVR_running import check_SteamVR_running
 
 def main():
+    check_SteamVR_running()
     get_SteamVR_path()
     action_selector()
 
